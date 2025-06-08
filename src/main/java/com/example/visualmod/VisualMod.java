c7c075b9368aa578ede2d05d03e9aa3f3fcf69f2
Kirill0538 <kshirshikov@yandex.ru>
2025-06-08 12:21:40 +0500

    Initial commit of visualmod

---
+++ src/main/java/com/example/visualmod/VisualMod.java
@@ -0,0 +1,10 @@
+package com.example.visualmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("visualmod")
+public class VisualMod {
+    public VisualMod() {
+        System.out.println("VisualMod loaded!");
+    }
+}
